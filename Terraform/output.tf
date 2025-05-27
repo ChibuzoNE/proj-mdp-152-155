2cc61b0fb9e5c7dbcb8c1c3b46c8e1f8778f7fcf
ChibuzoNE <nmechachibuzo@gmail.com>
2025-05-27 10:19:40 -0230

    Few deletion

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -28,11 +28,3 @@ description = "ID of the created VPC"
 value       = aws_vpc.main.id
 }
 
-output "k8s_master_ip" {
-value = aws_instance.k8s_master.public_ip
-}
-
-output "k8s_worker_ip" {
-value = aws_instance.k8s_worker.public_ip
-}
-
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,7 +7,7 @@ default     = "us-east-2"
 variable "instance_type" {
 description = "EC2 instance type"
 type        = string
-default     = "t2.medium"
+default     = "t2.micro"
 }
 
 variable "key_name" {
