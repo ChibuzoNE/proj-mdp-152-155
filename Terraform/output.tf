9145db06e20e74841a7d2bb88b24c0c4a039ee1e
ChibuzoNE <nmechachibuzo@gmail.com>
2025-05-27 08:26:00 -0230

    changes made today

---
+++ Terraform/output.tf
@@ -0,0 +1,38 @@
+output "build_server_public_ip" {
+description = "Public IP of the Build Server (Maven)"
+value       = aws_instance.build_server.public_ip
+}
+
+output "tomcat_server_public_ip" {
+description = "Public IP of the Tomcat Runtime Server"
+value       = aws_instance.tomcat_server.public_ip
+}
+
+output "build_server_ssh_command" {
+description = "SSH command to connect to the Build Server"
+value       = "ssh -i ${var.key_name}.pem ec2-user@${aws_instance.build_server.public_ip}"
+}
+
+output "tomcat_server_ssh_command" {
+description = "SSH command to connect to the Tomcat Server"
+value       = "ssh -i ${var.key_name}.pem ec2-user@${aws_instance.tomcat_server.public_ip}"
+}
+
+output "tomcat_app_url" {
+description = "URL to access the deployed application"
+value       = "http://${aws_instance.tomcat_server.public_ip}:8080/yourapp"
+}
+
+output "vpc_id" {
+description = "ID of the created VPC"
+value       = aws_vpc.main.id
+}
+
+output "k8s_master_ip" {
+value = aws_instance.k8s_master.public_ip
+}
+
+output "k8s_worker_ip" {
+value = aws_instance.k8s_worker.public_ip
+}
+
